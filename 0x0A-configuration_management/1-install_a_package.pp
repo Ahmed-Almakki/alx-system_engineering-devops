e3ab61206c552d9089857c1bedb140c0047d4342
Ahmed-Almakki <ahmedlmakkii@gmial.com>
2024-06-28 17:35:11 -0700

    add requitre

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,12 +2,13 @@
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3'],
 }
 
 package { 'Python':
   ensure   => '3.8.10',
-}
-
+} 
 package { 'Werkzeug':
   ensure => '2.1.1',
+  require  => Package['python3'],
 }

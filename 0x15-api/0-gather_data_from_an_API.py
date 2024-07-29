808b8f495ef5072bcaa190094b51d7e3e34b9b33
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-07-29 15:26:47 -0700

    first commit

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""returns information about his/her TODO list progress."""
+from requests import get
+import json
+import sys
+
+
+employ_id = sys.argv[1]
+
+user_resp = f'https://jsonplaceholder.typicode.com/users/{employ_id}'
+to_do_resp = f'https://jsonplaceholder.typicode.com/users/{employ_id}/todos'
+
+usr = get(user_resp)
+todo = get(to_do_resp)
+
+to_pars = json.loads(todo.text)
+pars = json.loads(usr.text)
+
+name = pars.get('name')
+titles = [i['title'] for i in to_pars if i['completed']]
+total_number = len(to_pars)
+number_done = len(titles)
+print("Employee {} is done with tasks({}/{}):".format(name, number_done, total_number))
+for t in titles:
+    print("     {}".format(t))

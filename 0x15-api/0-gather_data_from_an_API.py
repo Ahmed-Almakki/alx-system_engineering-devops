443f60ba9ec6a306ecb37e874365d4e286b73849
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-07-29 18:07:14 -0700

    s

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,27 +1,37 @@
 #!/usr/bin/python3
-"""This Script will fetch a user's todo list"""
+"""
+Script that, using this REST API, for a given employee ID, returns
+information about his/her TODO list progress
+"""
+
+import json
 import requests
-import sys
+from sys import argv
+
 
 if __name__ == "__main__":
-    entrypoint = "https://jsonplaceholder.typicode.com"
-    usrId = sys.argv[1]
-    try:
-        req_user = requests.get(
-            "{ep}/users/{usrId}".format(ep=entrypoint, usrId=usrId)
-        ).json()
-        if req_user:
-            tasks = requests.get(
-                "{ep}/todos".format(ep=entrypoint),
-                params={"userId": usrId}
-            ).json()
-            num_tasks = len(tasks)
-            fin_tasks = [t for t in tasks if t['completed'] is True]
-            print(
-                "Employee {} is done with tasks({}/{}):"
-                .format(req_user['name'], len(fin_tasks), num_tasks)
-            )
-            [print('\t ' + task['title']) for task in fin_tasks]
-    except Exception as e:
-        print(e)
 
+    sessionReq = requests.Session()
+
+    idEmp = argv[1]
+    idURL = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(idEmp)
+    nameURL = 'https://jsonplaceholder.typicode.com/users/{}'.format(idEmp)
+
+    employee = sessionReq.get(idURL)
+    employeeName = sessionReq.get(nameURL)
+
+    json_req = employee.json()
+    name = employeeName.json()['name']
+
+    totalTasks = 0
+
+    for done_tasks in json_req:
+        if done_tasks['completed']:
+            totalTasks += 1
+
+    print("Employee {} is done with tasks({}/{}):".
+          format(name, totalTasks, len(json_req)))
+
+    for done_tasks in json_req:
+        if done_tasks['completed']:
+            print("\t " + done_tasks.get('title'))

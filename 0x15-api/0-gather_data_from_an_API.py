6236ec449c590729b88e4ef0ebe7affa57986b2b
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-07-29 18:05:49 -0700

    s

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,14 +1,27 @@
 #!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
+"""This Script will fetch a user's todo list"""
 import requests
 import sys
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    entrypoint = "https://jsonplaceholder.typicode.com"
+    usrId = sys.argv[1]
+    try:
+        req_user = requests.get(
+            "{ep}/users/{usrId}".format(ep=entrypoint, usrId=usrId)
+        ).json()
+        if req_user:
+            tasks = requests.get(
+                "{ep}/todos".format(ep=entrypoint),
+                params={"userId": usrId}
+            ).json()
+            num_tasks = len(tasks)
+            fin_tasks = [t for t in tasks if t['completed'] is True]
+            print(
+                "Employee {} is done with tasks({}/{}):"
+                .format(req_user['name'], len(fin_tasks), num_tasks)
+            )
+            [print('\t ' + task['title']) for task in fin_tasks]
+    except Exception as e:
+        print(e)
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
